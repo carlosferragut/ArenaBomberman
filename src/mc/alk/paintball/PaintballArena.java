c7ff1109faaa7dc87a60791d03b3568196548638
alkarin <alkarin.v@gmail.com>
2013-03-29 12:33:06 -0500

    Revert back from a bad accidental commit

---
+++ src/mc/alk/paintball/PaintballArena.java
@@ -0,0 +1,30 @@
+package mc.alk.paintball;
+
+import mc.alk.arena.objects.arenas.Arena;
+import mc.alk.arena.objects.events.MatchEventHandler;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class PaintballArena extends Arena{
+	static int damage = 20;
+
+	/**
+	 * This is how you create customized events.  You specify a method as a @MatchEventHandler
+	 * and give it at least one bukkit event as the first argument.  In this case its EntityDamageByEntityEvent
+	 * These events will ONLY be called when a match is ongoing
+	 * If the event returns a player (in this case it does) then the event only gets called when
+	 * 1) match is ongoing
+	 * 2) player is still alive in the match
+	 *
+	 * @param event: Which bukkit event are we listening to
+	 */
+	@MatchEventHandler(suppressCastWarnings=true)
+	public void onEntityDamage(EntityDamageByEntityEvent event) {
+		if (event.isCancelled())
+			return;
+		if (event.getDamager().getType() != EntityType.SNOWBALL)
+			return;
+		event.setDamage(damage);
+	}
+}
